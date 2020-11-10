a2b0bf7b52db7e835c3131164cee6e8911ca7112
Sourav Rakshit <rakshitsourav3@gmail.com>
2020-11-10 20:04:30 +0530

    Added the importHandler logic

---
+++ src/controllers/importHandler.js
@@ -0,0 +1,27 @@
+import Axios from "axios";
+
+const URL = `https://codeditorapi.azurewebsites.net/share/import`;
+
+const importHandler = (id, isLoading, setCode, setInput, setLanguage, setShow) => {
+    Axios.post(URL, {
+        id: id,
+    })
+        .then((response) => {
+            isLoading(false);
+            if (response.data.success) {
+                console.log(response.data.data);
+                const {code, input, language} = response.data.data;
+                setCode(code);
+                setInput(input);
+                setLanguage(language);
+                isLoading(false);
+                setShow(false);
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+            return "";
+        });
+};
+
+export default importHandler;
