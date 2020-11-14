e362468082b0a2f210d11db6abbc26e60e6fde1a
Sourav Rakshit <rakshitsourav3@gmail.com>
2020-11-15 01:45:32 +0530

    Added ascii art in console

--- src/index.js
+++ src/index.js
@@ -1,39 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
 
 // BaseWeb imports
-import { Client as Styletron } from 'styletron-engine-atomic';
-import { Provider as StyletronProvider } from "styletron-react"
+import { Client as Styletron } from "styletron-engine-atomic";
+import { Provider as StyletronProvider } from "styletron-react";
 const engine = new Styletron();
 
-
 // REDUX
-import { store, persistor } from './app/store'; // persistor is from redux-persist
-import { Provider } from 'react-redux';
-
+import { store, persistor } from "./app/store"; // persistor is from redux-persist
+import { Provider } from "react-redux";
 
 // Redux Persistence
-import { PersistGate } from 'redux-persist/integration/react';
+import { PersistGate } from "redux-persist/integration/react";
 
 // Components/Containers Imports
-import App from './App';
+import App from "./App";
 
 // ReactDOM Render
 ReactDOM.render(
-  <Provider store={store}>
-    <StyletronProvider value={engine}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </StyletronProvider>
-  </Provider>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <StyletronProvider value={engine}>
+            <PersistGate loading={null} persistor={persistor}>
+                <App />
+            </PersistGate>
+        </StyletronProvider>
+    </Provider>,
+    document.getElementById("root")
 );
-
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// Codeditor logo on console
+console.log(
+    `
+Thank you for using
+██████╗ ██████╗ ██████╗ ███████╗██████╗ ██╗████████╗ ██████╗ ██████╗ 
+██╔════╝██╔═══██╗██╔══██╗██╔════╝██╔══██╗██║╚══██╔══╝██╔═══██╗██╔══██╗
+██║     ██║   ██║██║  ██║█████╗  ██║  ██║██║   ██║   ██║   ██║██████╔╝
+██║     ██║   ██║██║  ██║██╔══╝  ██║  ██║██║   ██║   ██║   ██║██╔══██╗
+╚██████╗╚██████╔╝██████╔╝███████╗██████╔╝██║   ██║   ╚██████╔╝██║  ██║
+ ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═════╝ ╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝
+Have fun...
+`
+);
