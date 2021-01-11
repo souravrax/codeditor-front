e50d9b045a2cb053004f74432555bd08a7cd00c2
Sourav Rakshit <rakshitsourav3@gmail.com>
2021-01-12 01:47:08 +0530

    Fixed #2

--- src/app/store.js
+++ src/app/store.js
@@ -2,15 +2,14 @@ import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./root-reducer";
 
 import { persistStore } from "redux-persist";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const middleware = []; // no middleware
 
 // Reference: https://github.com/zalmoxisus/redux-devtools-extension#12-advanced-store-setup
-const composeEnhancer = window
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    : compose;
+// const composeEnhancer = window ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose; // this is a bug
 
-const enhancer = composeEnhancer(applyMiddleware(...middleware));
+const enhancer = composeWithDevTools(applyMiddleware(...middleware));
 
 export const store = createStore(rootReducer, enhancer);
 export const persistor = persistStore(store); // Reference: https://www.npmjs.com/package/redux-persist
